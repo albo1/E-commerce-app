0e17a80821e290aadb4f3bdc0bcc59e53ba4afc9
alboy77 <al.boucetta@yahoo.com>
2023-04-30 16:00:41 -0400

    huh

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,19 +6,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', async (req, res) => {
   try {
-    const categoryData = await Category.findAll({
-      include: [{model: Product},]
+    const productData = await Product.findAll({
+      include: [Category, {model: Tag, through: ProductTag},]
     });
-    res.status(200).json(categoryData);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [Category, {model: Tag, through: ProductTag},]
+    });
+
+    if(!productData) {
+      res.status(404).json({message: 'NO PRODUCT FOUND'});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
